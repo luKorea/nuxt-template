9ca4b57e33d0c14ce5187bc6914a4dc534145b14
korealu <643949593@qq.com>
2023-03-10 11:29:39 +0800

    新增全局 SEO 配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "OPPO手机商城",
+      meta: [
+        {
+          name: "description",
+          content:
+            "OPPO专区，官方正品，最新最全的OPPO手机产品以及配件在线抢购！",
+        },
+        {
+          name: "keywords",
+          content: "OPPO商城，OPPO专区, OPPO手机，OPPO配件，OPPO, OPPO官网商城",
+        },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      noscript: [{ children: "Javascript is required" }],
+    },
+  },
   css: ["normalize.css", "@/assets/css/global.scss"],
   vite: {
     css: {
