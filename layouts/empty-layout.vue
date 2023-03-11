e5613081d5b2b13b43fde8a932c69ec4e2075234
korealu <643949593@qq.com>
2023-03-11 08:25:42 +0800

    fetch封装

--- layouts/empty-layout.vue
+++ layouts/empty-layout.vue
@@ -4,4 +4,10 @@
     <slot></slot>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+// 通过 definePageMeta 配置一些页面使用空白布局
+// definePageMeta({
+//   layout:'empty-layout'
+// })
+</script>
